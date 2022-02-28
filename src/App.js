c910b298776018599b580d8c68a2e8c203a53262
s18914 <s18914@pjwstk.edu.pl>
2022-02-28 23:38:11 +0100

    add components header and button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import React from 'react'
+import Button from './components/Button'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Button color='green'/>
+      <Button color='red' text=':('/>
+      <Button color='blue'/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='App-header'>
+        <h1>Hejka!</h1>
+    </header>
+  )
+}
+
+export default Header
